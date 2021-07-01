07fb6404fa3311cf60975f5de51fa54879b7c876
Ayham-Almazari <76968009+Ayham-Almazari@users.noreply.github.com>
2021-07-01 19:22:25 +0300

    complete teacher classrooms controoler

--- app/Http/Controllers/API/TeacherControllers/TeachersClassesController.php
+++ app/Http/Controllers/API/TeacherControllers/TeachersClassesController.php
@@ -3,25 +3,37 @@
 namespace App\Http\Controllers\API\TeacherControllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ClassRoomResource;
 use App\Models\Teacher;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
 
 class TeachersController extends Controller
 {
+    private  $Teacher;
+    private $Teacher_classes;
+
+    public function __construct()
+    {
+        $this->Teacher = auth('teacher')->user();
+        $this->Teacher_classes = $this->Teacher->Teacher_classes;
+    }
+
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return AnonymousResourceCollection
      */
     public function index()
     {
-        //
+        return ClassRoomResource::collection($this->Teacher_classes);
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
@@ -32,7 +44,7 @@ class TeachersController extends Controller
      * Store a newly created resource in storage.
      *
      * @param Request $request
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function store(Request $request)
     {
@@ -43,7 +55,7 @@ class TeachersController extends Controller
      * Display the specified resource.
      *
      * @param Teacher $teacher
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function show(Teacher $teacher)
     {
@@ -54,7 +66,7 @@ class TeachersController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param Teacher $teacher
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function edit(Teacher $teacher)
     {
@@ -66,7 +78,7 @@ class TeachersController extends Controller
      *
      * @param Request $request
      * @param Teacher $teacher
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function update(Request $request, Teacher $teacher)
     {
@@ -77,7 +89,7 @@ class TeachersController extends Controller
      * Remove the specified resource from storage.
      *
      * @param Teacher $teacher
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy(Teacher $teacher)
     {
--- resources/js/components/nav_bar.vue
+++ resources/js/components/nav_bar.vue
@@ -3,7 +3,14 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{showTitle}}</div>
+                    <router-link :to="{ name : 'home' }">Home</router-link>
+                    <a v-if="check">
+                        <router-link :to="{ name : 'login' }"> Log In </router-link>
+                    </a>
+                    <a v-if="check">
+                        <router-link :to="{ name : 'register' }"> Sign Up </router-link>
+                    </a>
+                    <router-link :to="{ name : 'TeacherIndex' }"> TeacherIndex </router-link>
 
                     <div class="card-body">
                         I'm an example component.
